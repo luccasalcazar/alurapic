81a25e5fe73df345251cd4d1e86ace218a595a3b
Lucas Alcazar <luccasalcazar@gmail.com>
2020-12-06 04:37:05 -0300

    atualizando github

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+
 import { Photo } from "./photo";
 
 const API = 'http://localhost:3000';
@@ -16,4 +17,11 @@ export class PhotoService {
         return this.http
         .get<Photo[]>(API + '/' + userName + '/photos');
     }
-}
+
+    listFromUserPaginated(userName: string, page: number) {
+        const params = new HttpParams()
+            .append('page', page.toString());
+        return this.http
+        .get<Photo[]>(API + '/' + userName + '/photos', { params });
+    }
+} 

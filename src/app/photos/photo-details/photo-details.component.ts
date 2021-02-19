1b6143cd59ecf78053fca2789c14d990f2ced258
Lucas Alcazar <luccasalcazar@gmail.com>
2021-02-18 23:50:49 -0300

    atualizando github

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,24 +1,43 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Observable } from "rxjs";
+import { AlertService } from "src/app/shared/components/alert/alert.service";
+
 import { Photo } from "../photo/photo";
 import { PhotoService } from "../photo/photo.service";
 
 @Component({
     templateUrl: './photo-details.component.html',
-    styleUrls: ['photo-details.css']
 })
 export class PhotoDetailsComponent implements OnInit { 
 
     photo$: Observable<Photo>;
+    photoId: number
+    
     constructor(
         private route: ActivatedRoute,
-        private photoService: PhotoService
+        private photoService: PhotoService,
+        private router: Router,
+        private alertService: AlertService
     ) { }
 
     ngOnInit(): void {
-        this.photo$ = this.photoService.findById(
-            this.route.snapshot.params.photoId
-        )
+        this.photoId = this.route.snapshot.params.photoId;
+        this.photo$ = this.photoService.findById(this.photoId);
+    }
+
+    remove() {
+        this.photoService
+            .removePhoto(this.photoId)
+            .subscribe(
+                () => {
+                    this.alertService.success('Photo removed');
+                    this.router.navigate(['']);
+                },
+                err => {
+                    console.log(err);
+                    this.alertService.warning('Could not delete the photo!')
+                    
+                });
     }
 }
